8c98d07f3fa3c06dec97833df10b634fe017533e
illright <leva181777@gmail.com>
2019-10-16 21:22:32 +0300

    Add uWSGI to the dependencies

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 """Run script"""
 
+import os
+
 from innopoints.app import create_app
 
 
 if __name__ == '__main__':
-    app = create_app('config/dev.py')  # pylint: disable=invalid-name
-    app.run(host='0.0.0.0', port=7507)
+    app = create_app('config/prod.py')  # pylint: disable=invalid-name
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 7507))
 else:
     app = create_app()  # pylint: disable=invalid-name
