67586f11d1af4df4e0e80471f583ab6c264c19ec
illright <leva181777@gmail.com>
2019-11-11 15:16:32 +0300

    Remove the migrations module altogether

--- innopoints/app.py
+++ innopoints/app.py
@@ -3,8 +3,6 @@
 import os
 
 from flask import Flask
-import flask_migrate
-from flask_migrate import Migrate
 
 from innopoints.views import api, oauth
 from innopoints.models import db, login_manager
@@ -17,13 +15,9 @@ def create_app(config='config/prod.py'):
     app.config.from_pyfile(config)
 
     db.init_app(app)
-    Migrate(app, db)
 
     with app.app_context():
-        if not os.path.exists('migrations'):
-            flask_migrate.init()
-        flask_migrate.migrate()
-        flask_migrate.upgrade()
+        db.create_all()
 
     login_manager.init_app(app)
 
