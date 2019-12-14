56fe5c157790a9e2559cb1284d741444ffcec75e
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-12-14 18:00:54 +0300

    Separate "Project" model

--- innopoints/models/models.py
+++ innopoints/models/models.py
@@ -14,20 +14,6 @@ DEFAULT_QUESTIONS = ("What did you learn from this volunteering opportunity?",
 
 # TODO: set passive_deletes
 
-class ReviewStatus(Enum):
-    """Represents the review status of the project"""
-    pending = auto()
-    approved = auto()
-    rejected = auto()
-
-
-class LifetimeStage(Enum):
-    """Represents the project's lifetime stage"""
-    draft = auto()
-    ongoing = auto()
-    past = auto()
-
-
 class ApplicationStatus(Enum):
     """Represents volunteering application's status"""
     approved = auto()
@@ -56,54 +42,6 @@ class NotificationType(Enum):
     out_of_stock = auto()
     new_purchase = auto()
     proj_final_review = auto()
-
-
-project_moderation = db.Table(
-    'project_moderation',
-    db.Column('project_id', db.Integer,
-              db.ForeignKey('projects.id', ondelete='CASCADE'),
-              primary_key=True),
-    db.Column('account_email', db.String(128),
-              db.ForeignKey('accounts.email', ondelete='CASCADE', onupdate='CASCADE'),
-              primary_key=True)
-)
-
-
-class Project(db.Model):
-    """Represents a project for volunteering"""
-    __tablename__ = 'projects'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False, unique=True)
-    image_id = db.Column(db.Integer, db.ForeignKey('static_files.id'), nullable=True)
-    creation_time = db.Column(db.DateTime, default=datetime.utcnow)
-    organizer = db.Column(db.String(128), nullable=True)
-    activities = db.relationship('Activity',
-                                 cascade="all, delete-orphan",
-                                 passive_deletes=True,
-                                 backref='project')
-    moderators = db.relationship('Account',
-                                 secondary=project_moderation,
-                                 backref=db.backref('moderated_projects',
-                                                    lazy=True))
-    creator_email = db.Column(db.String(128), db.ForeignKey('accounts.email'), nullable=False)
-    # property `creator` created with a backref
-    admin_feedback = db.Column(db.String(1024), nullable=True)
-    review_status = db.Column(db.Enum(ReviewStatus), nullable=True)
-    lifetime_stage = db.Column(db.Enum(LifetimeStage), nullable=False)
-
-    files = db.relationship('ProjectFile',
-                            cascade='all, delete-orphan',
-                            backref='project')
-    notifications = db.relationship('Notification',
-                                    cascade='all, delete-orphan')
-
-    @property
-    def image_url(self):
-        """Return an image URL constructed from the ID"""
-        if self.image_id is None:
-            return None
-        return f'/file/{self.image_id}'
 
 
 class Activity(db.Model):
