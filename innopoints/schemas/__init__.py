3ab5fe9b6e997a4d4179ee5b49adee81221a270b
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-12-16 22:39:40 +0200

    Remove empty schemas.py

--- innopoints/schemas/__init__.py
+++ innopoints/schemas/__init__.py
@@ -4,5 +4,3 @@ from .activity import *
 from .application import *
 from .product import *
 from .variety import *
-
-from .schemas import *  # temporary until moved all schemas
--- innopoints/schemas/schemas.py
+++
@@ -1,9 +0,0 @@
-"""Model schemas for serialization."""
-
-from marshmallow import ValidationError, pre_load, post_dump
-from innopoints.extensions import ma, db
-
-
-# pylint: disable=missing-docstring
-# pylint: disable=no-member
-
