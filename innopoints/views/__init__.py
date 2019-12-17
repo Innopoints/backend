2a9099b20d1dd054c917668f676079b65ea0254d
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-12-17 22:11:54 +0200

    Separate "Product" view

--- innopoints/views/__init__.py
+++ innopoints/views/__init__.py
@@ -2,5 +2,6 @@ from .authentication import *
 from .project import *
 from .activity import *
 from .file import *
+from .product import *
 
 from .views import *  # temporary, until all views have been moved
