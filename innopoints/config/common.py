f3d4ef19a18d3db5490318b4601b0f64c088ae09
illright <leva181777@gmail.com>
2019-12-22 23:41:41 +0500

    Fix the warnings in configs

--- innopoints/config/common.py
+++ innopoints/config/common.py
@@ -1,4 +1,7 @@
+"""The base application configuration."""
+
 import os
+
 
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- innopoints/config/prod.py
+++ innopoints/config/prod.py
@@ -1,13 +1,14 @@
-"""Application configuration for production"""
+"""Application configuration for production."""
 
 import os
 from innopoints.config.common import *
 
+
 INNOPOLIS_SSO_CLIENT_ID = os.environ['INNOPOLIS_SSO_CLIENT_ID']
 INNOPOLIS_SSO_CLIENT_SECRET = os.environ['INNOPOLIS_SSO_CLIENT_SECRET']
 
-def is_admin(userinfo):
-    """Determine if the user is an administrator by a dictionary of claims"""
+def is_admin(userinfo):  # pylint: disable=unused-argument
+    """Determine if the user is an administrator by a dictionary of claims."""
     return False
 
 IS_ADMIN = is_admin
