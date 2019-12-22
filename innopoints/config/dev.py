3dcd2aca1867cfb4bb23d06717a48b2167e8aad4
illright <leva181777@gmail.com>
2019-12-22 23:41:41 +0500

    Fix the warnings in configs

--- innopoints/config/dev.py
+++ innopoints/config/dev.py
@@ -1,15 +1,12 @@
-"""Application configuration for development"""
+"""Application configuration for development."""
 
 import os
 from innopoints.config.common import *
 
-INNOPOLIS_SSO_CLIENT_ID = os.environ.get('INNOPOLIS_SSO_CLIENT_ID')
-INNOPOLIS_SSO_CLIENT_SECRET = os.environ.get('INNOPOLIS_SSO_CLIENT_SECRET')
+
+INNOPOLIS_SSO_CLIENT_ID = os.getenv('INNOPOLIS_SSO_CLIENT_ID')
+INNOPOLIS_SSO_CLIENT_SECRET = os.getenv('INNOPOLIS_SSO_CLIENT_SECRET')
 
 JSON_SORT_KEYS = False
 
-def is_admin(userinfo):
-    """Determine if the user is an administrator by a dictionary of claims"""
-    return True
-
-IS_ADMIN = is_admin
+IS_ADMIN = lambda userinfo: True
