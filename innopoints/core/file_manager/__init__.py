aa8470de753c74f641c539f0fe3583cb293a7fc1
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-12-22 15:44:27 +0200

    Use appropriate File Manager depending on env var

--- innopoints/core/file_manager/__init__.py
+++ innopoints/core/file_manager/__init__.py
@@ -1,5 +1,9 @@
 """File manager module."""
+import os
 from .s3 import FileManagerS3
 from .local import FileManagerLocal
 
-file_manager = FileManagerLocal()
+if int(os.getenv('USE_S3', '0')) == 1:
+    file_manager = FileManagerS3()
+else:
+    file_manager = FileManagerLocal()
