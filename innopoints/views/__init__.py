64d1d58b93c4341e58be433693460a18212f66e9
illright <leva181777@gmail.com>
2019-12-22 23:46:10 +0500

    Add the forgotten docstring

--- innopoints/views/__init__.py
+++ innopoints/views/__init__.py
@@ -1,6 +1,8 @@
-from .authentication import *
-from .project import *
+"""This module contains the views (API endpoints)."""
+
 from .activity import *
+from .authentication import *
 from .file import *
 from .product import *
+from .project import *
 from .variety import *
