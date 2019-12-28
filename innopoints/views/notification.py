f0f4f88c02c2faa8dcb12acee87ea313b20a01b2
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-12-28 15:57:21 +0200

    Mark notification as read (endpoint)

--- innopoints/views/notification.py
+++ innopoints/views/notification.py
@@ -1,8 +1,10 @@
 """Views related to notifications."""
 
+from flask import abort
 from flask_login import login_required, current_user
 
 from innopoints.blueprints import api
+from innopoints.extensions import db
 from innopoints.models import Notification
 from innopoints.schemas import NotificationSchema
 
@@ -15,3 +17,15 @@ def get_notifications():
     notifications = query.all()
     schema = NotificationSchema(many=True)
     return schema.jsonify(notifications)
+
+@api.route('/notifications/<int:notification_id>/read', methods=['PATCH'])
+@login_required
+def read_notification(notification_id):
+    """Marks a notification as read."""
+    notification = Notification.query.get_or_404(notification_id)
+    if notification.recipient_email != current_user.email:
+        abort(401, {'message': 'Not your notification'})
+    notification.is_read = True
+    db.session.add(notification)
+    db.session.commit()
+    return NotificationSchema().jsonify(notification)
