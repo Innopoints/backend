8ca4a372d197cfea1430cdfbd51d423bb197b18e
illright <leva181777@gmail.com>
2020-01-04 00:01:02 +0500

    Add volunteer report gathering

--- innopoints/views/application.py
+++ innopoints/views/application.py
@@ -5,12 +5,13 @@ Application:
 - DELETE /projects/{project_id}/activities/{activity_id}/applications
 
 VolunteeringReport:
+- GET /projects/{project_id}/activities/{activity_id}/applications/{application_id}/report_info
 - POST /projects/{project_id}/activities/{activity_id}/applications/{application_id}/report
 """
 
 import logging
 
-from flask import abort, request
+from flask import abort, request, jsonify
 from flask_login import login_required, current_user
 from marshmallow import ValidationError
 from sqlalchemy.exc import IntegrityError
@@ -23,6 +24,8 @@ from innopoints.models import (
     ApplicationStatus,
     LifetimeStage,
     Project,
+    project_moderation,
+    VolunteeringReport,
 )
 from innopoints.schemas import ApplicationSchema, VolunteeringReportSchema
 
@@ -92,6 +95,43 @@ def take_back_application(project_id, activity_id):
 
 
 # ----- VolunteeringReport -----
+
+@api.route('/projects/<int:project_id>/activities/<int:activity_id>'
+           '/applications/<int:application_id>/report_info')
+@login_required
+def get_report_info(project_id, activity_id, application_id):
+    """Get the reports from the moderators of the project and an average rating."""
+    application = Application.query.get_or_404(application_id)
+    activity = Activity.query.get_or_404(activity_id)
+    project = Project.query.get_or_404(project_id)
+
+    if activity.project != project or application.activity_id != activity.id:
+        abort(400, {'message': 'The specified project, activity and application are unrelated.'})
+
+    if current_user not in project.moderators and not current_user.is_admin:
+        abort(401)
+
+    avg_rating = db.session.query(
+        db.func.round(db.func.avg(VolunteeringReport.rating))
+    ).join(Application).join(Activity).join(
+        project_moderation,
+        VolunteeringReport.reporter_email == project_moderation.c.account_email
+    ).filter(
+        Application.applicant_email == application.applicant_email,
+        project_moderation.c.project_id == project_id,
+    ).scalar() or 0
+
+    reports = VolunteeringReport.query.join(Application).join(Activity).join(
+        project_moderation,
+        VolunteeringReport.reporter_email == project_moderation.c.account_email
+    ).filter(
+        Application.applicant_email == application.applicant_email,
+        project_moderation.c.project_id == project_id,
+    ).all()
+
+    out_schema = VolunteeringReportSchema(only=('content', 'rating', 'time'), many=True)
+    return jsonify(average_rating=int(avg_rating), reports=out_schema.dump(reports))
+
 
 @api.route('/projects/<int:project_id>/activities/<int:activity_id>'
            '/applications/<int:application_id>/report', methods=['POST'])
