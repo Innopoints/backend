68423d3afe34d0c8591d0833ee21451446cfc957
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-02-11 20:15:58 +0300

    Fix production config URLs

--- innopoints/config/prod.py
+++ innopoints/config/prod.py
@@ -9,14 +9,15 @@ SECRET_KEY = os.environ['SECRET_KEY']
 INNOPOLIS_SSO_CLIENT_ID = os.environ['INNOPOLIS_SSO_CLIENT_ID']
 INNOPOLIS_SSO_CLIENT_SECRET = os.environ['INNOPOLIS_SSO_CLIENT_SECRET']
 
-CORS_ORIGINS = ['https://innopoints-frontend.herokuapp.com']
+CORS_ORIGINS = []
 
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SAMESITE = None
 REMEMBER_COOKIE_SECURE = True
 
-FRONTEND_BASE = 'https://innopoints-frontend.herokuapp.com'
+FRONTEND_BASE = 'https://ipts.innopolis.university'
+
 
 def is_admin(userinfo):  # pylint: disable=unused-argument
     """Determine if the user is an administrator by a dictionary of claims."""
