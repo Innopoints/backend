71060761f58e358dc313d3e7957d0f1cef76e7d7
illright <leva181777@gmail.com>
2020-03-31 01:28:16 +0300

    Use the Flask-pyWebPush library

--- innopoints/extensions.py
+++ innopoints/extensions.py
@@ -4,7 +4,6 @@ To avoid circular imports with views and create_app(), extensions are instantiat
 They will be initialized (calling init_app()) in app.py.
 """
 
-import json
 import os
 
 from authlib.integrations.flask_client import OAuth
@@ -12,29 +11,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-from flask import current_app
-from pywebpush import webpush, WebPushException
-
-
-class WebPush:
-    def __init__(self, app=None):
-        self.public_key = None
-        self.private_key = None
-        self.sender_info = None
-
-        if app is not None:
-            self.init_app(app)
-
-    def init_app(self, app):
-        self.public_key = app.config.get('WEBPUSH_VAPID_PUBLIC_KEY')
-        self.private_key = app.config.get('WEBPUSH_VAPID_PRIVATE_KEY')
-        self.sender_info = app.config.get('WEBPUSH_SENDER_INFO')
-
-    def send(self, subscription, notification):
-        webpush(subscription,
-                json.dumps(notification),
-                vapid_private_key=self.private_key,
-                vapid_claims={"sub": self.sender_info})
+from flask_pywebpush import WebPush
 
 
 db = SQLAlchemy()
