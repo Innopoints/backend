1a5a37b9869e3d56dfa676fdfed4008732d43312
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-04-18 23:37:47 +0300

    Remove S3 and base FileManager classes

--- innopoints/core/file_manager/__init__.py
+++ innopoints/core/file_manager/__init__.py
@@ -1,11 +1,4 @@
 """File manager module."""
-import os
-
-from .s3 import FileManagerS3
 from .local import FileManagerLocal
 
-
-if int(os.getenv('USE_S3', '0')) == 1:
-    file_manager = FileManagerS3()
-else:
-    file_manager = FileManagerLocal()
+file_manager = FileManagerLocal()
