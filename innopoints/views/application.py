87d42260c1eac4753e984ef389f82c81495b0699
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-04-26 16:24:33 +0300

    Allow changing hours of fixed-reward applications Must be either 0 or 1

--- innopoints/views/application.py
+++ innopoints/views/application.py
@@ -165,9 +165,9 @@ def edit_application(project_id, activity_id, application_id):
         if not isinstance(actual_hours, int) or actual_hours < 0:
             abort(400, {'message': 'Actual hours must be a non-negative integer.'})
 
-        if activity.fixed_reward:
-            abort(400, {'message': 'Working hours may only be changed '
-                                   'on hourly-rate activity applications.'})
+        if activity.fixed_reward and actual_hours not in [0, 1]:
+            abort(400, {'message': 'Working hours on hourly-rate activities'
+                                   'may only be set to 0 or 1.'})
 
         if application.status != ApplicationStatus.approved:
             abort(400, {'message': 'Working hours may only be changed on approved applications.'})
