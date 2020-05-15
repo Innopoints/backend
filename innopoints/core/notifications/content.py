f30888ad17df2eae9ea85619bfa77e34c9d6f629
illright <leva181777@gmail.com>
2020-05-15 22:07:19 +0300

    Refactor the notifications module

---
+++ innopoints/core/notifications/content.py
@@ -0,0 +1,157 @@
+"""Functionality for assembling notification content."""
+
+from dataclasses import dataclass
+
+from flask import current_app
+
+from innopoints.models import Product, NotificationType, StockChangeStatus, ApplicationStatus
+from innopoints.schemas import PayloadSchema
+
+
+@dataclass
+class Link:
+    """The representation of a link in notification bodies."""
+    title: str
+    href: str
+
+    def __str__(self):
+        """Return the <a> tag for the link."""
+        return f'<a href="{current_app.config["FRONTEND_BASE"]}{self.href}">{self.title}</a>'
+
+
+# pylint: disable=invalid-name
+def s(amount):
+    """Return the correct pluralization of the amount of items."""
+    return 's' if amount != 1 else ''
+
+
+def product_repr(product: Product):
+    """A pretty string representation of a Product model instance."""
+    if product.type is None:
+        return product.name
+    return f"'{product.name}' {product.type}"
+
+
+# pylint: disable=too-many-branches
+def get_content(type: NotificationType, payload: dict):
+    """Given notification type and payload, returns the data for the notification."""
+    payload = payload and PayloadSchema().fill_data(payload.copy())
+
+    title: str = None
+    link: str = None
+    image_src: str = None
+    body: list = None
+
+    if type == NotificationType.purchase_status_changed:
+        title = 'Purchase status changed'
+        link = f"/products/{payload['product'].id}"
+        if payload['variety'].images:
+            image_src = payload['variety'].images[0]
+
+        body = ['Your ', product_repr(payload['product']), ' purchase ']
+
+        if payload['stock_change'].status == StockChangeStatus.ready_for_pickup.name:
+            body.append('is ready to be picked up at 319 Office.')
+        elif payload['stock_change'].status == StockChangeStatus.rejected.name:
+            body.append('was rejected.')
+        elif payload['stock_change'].status == StockChangeStatus.carried_out.name:
+            body.append('was delivered.')
+        else:
+            body.append('is being processed again.')
+    elif type == NotificationType.new_arrivals:
+        title = 'New arrivals'
+        link = '/store'
+        body = ['New products have arrived at the InnoStore.']
+    elif type == NotificationType.application_status_changed:
+        title = 'Application status changed'
+        link = f"/projects/{payload['project'].id}"
+        image_src = f"/file/{payload['project'].image_id}"
+        body = ['Your volunteering application for ',
+                Link(title=payload['activity'].name,
+                     href=f"/projects/{payload['project'].id}"),
+                ' was ']
+        if payload['application'].status == ApplicationStatus.approved:
+            body.append('approved.')
+        elif payload['application'].status == ApplicationStatus.rejected:
+            body.append('rejected.')
+        else:
+            body.append('moved back to pending.')
+    elif type == NotificationType.claim_innopoints:
+        _reward = payload['application'].actual_hours * payload['activity'].reward_rate
+        title = 'Claim your reward!' if _reward != 0 else 'Leave feedback'
+        link = f"/projects/{payload['project'].id}"
+        image_src = f"/file/{payload['project'].image_id}"
+        body = ['Leave feedback on your volunteering work']
+        if _reward != 0:
+            body.append(f' to claim {_reward} innopoint{s(_reward)}.')
+        else:
+            body.append('.')
+    elif type == NotificationType.project_review_status_changed:
+        title = 'An administrator has reviewed your project'
+        link = f"/projects/{payload['project'].id}"
+        image_src = f"/file/{payload['project'].image_id}"
+        body = [f"The project {payload['project'].name} was "
+                f"{payload['project'].review_status} by the administrator."]
+    elif type == NotificationType.added_as_moderator:
+        title = 'Moderator rights granted'
+        link = f"/projects/{payload['project'].id}"
+        image_src = f"/file/{payload['project'].image_id}"
+        body = [f'You have been promoted to moderate ',
+                Link(title=payload['project'].name,
+                     href=f"/projects/{payload['project'].id}"),
+                '.']
+    elif type == NotificationType.all_feedback_in:
+        title = 'All feedback collected'
+        link = f"/projects/{payload['project'].id}"
+        image_src = f"/file/{payload['project'].image_id}"
+        body = [f'All volunteers of ',
+                Link(title=payload['project'].name,
+                     href=f"/projects/{payload['project'].id}"),
+                ' have submitted feedback.']
+    elif type == NotificationType.out_of_stock:
+        title = 'Out of stock'
+        link = f"/products/{payload['product'].id}"
+        if payload['variety'].images:
+            image_src = payload['variety'].images[0]
+        body = ['The ',
+                Link(title=product_repr(payload['product']),
+                     href=f"/products/{payload['product'].id}"),
+                ' was sold out.']
+    elif type == NotificationType.new_purchase:
+        title = 'New purchase'
+        link = '/dashboard'
+        if payload['variety'].images:
+            image_src = payload['variety'].images[0]
+        body = [Link(title=payload['account'].full_name,
+                     href='/profile?user=' + payload['account'].email),
+                ' has purchased the ',
+                Link(title=product_repr(payload['product']),
+                     href=f"/products/{payload['product'].id}"),
+                '.']
+    elif type == NotificationType.project_review_requested:
+        title = 'Project review requested'
+        link = f"/projects/{payload['project'].id}"
+        image_src = f"/file/{payload['project'].image_id}"
+        body = ['The project ',
+                Link(title=payload['project'].name,
+                     href=f"/projects/{payload['project'].id}"),
+                ' is ready for review.']
+    elif type == NotificationType.service:
+        title = "Administrator's message"
+        body = payload['message']
+    elif type == NotificationType.manual_transaction:
+        title = 'Your balance has changed'
+        body = ['The administrator ']
+        if payload['transaction'].change > 0:
+            body.append(f"granted {payload['transaction'].change} innopoints to you.")
+        else:
+            body.append(f"deducted {-payload['transaction'].change} innopoints from you.")
+    else:
+        title = "Don't mind me"
+        body = ['Just a message from heaven wishing you a great day!']
+
+    return {'title': title,
+            'body': body,
+            'link': link,
+            'image': image_src,
+            'type': type.value}
--- innopoints/core/notifications/push.py
+++ innopoints/core/notifications/push.py
@@ -7,119 +7,20 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.attributes import flag_modified
 
 from innopoints.extensions import push as webpush, db
-from innopoints.models import NotificationType, Account, StockChangeStatus, ApplicationStatus
+from innopoints.models import NotificationType, Account
 from innopoints.schemas import PayloadSchema
+from .content import get_content, Link
+
 
 log = logging.getLogger(__name__)
 
 
-def get_content(notification_type, payload):
-    '''Given notification type and payload, returns the data for the notification'''
-    # pylint: disable=too-many-branches
-    title = 'Innopoints notification'
-    body = 'Something happened.'
-    link = '/'
-    image = None
+def remove_links(fragment):
+    """Replace Link objects with their titles (no links allowed in push notifications)."""
+    if isinstance(fragment, Link):
+        return fragment.title
+    return fragment
 
-    if notification_type == NotificationType.purchase_status_changed:
-        title = 'Purchase status changed'
-        if payload['product']['type'] is None:
-            purchase = payload['product']['name']
-        else:
-            purchase = f"'{payload['product']['name']}' {payload['product']['type']}"
-        if len(payload['variety']['images']) > 0:
-            image = payload['variety']['images'][0]
-        if payload['stock_change']['status'] == StockChangeStatus.ready_for_pickup.name:
-            status_update = 'is ready to be picked up at 319 Office.'
-        elif payload['stock_change']['status'] == StockChangeStatus.rejected.name:
-            status_update = 'was rejected.'
-        elif payload['stock_change']['status'] == StockChangeStatus.carried_out.name:
-            status_update = 'was delivered.'
-        else:
-            status_update = 'is being processed again.'
-        body = f'Your {purchase} purchase {status_update}'
-        link = f'/products/{payload["product"]["id"]}'
-    elif notification_type == NotificationType.new_arrivals:
-        title = 'New arrivals'
-        body = 'New products have arrived at the InnoStore'
-        link = '/store'
-    elif notification_type == NotificationType.application_status_changed:
-        title = 'Application status changed'
-        if payload['application']['status'] == ApplicationStatus.approved.name:
-            status = 'approved'
-        elif payload['application']['status'] == ApplicationStatus.rejected.name:
-            status = 'rejected'
-        else:
-            status = 'moved back to pending'
-        image = f'/file/{payload["project"]["image_id"]}'
-        body = f'Your volunteering application for {payload["activity"]["name"]} was {status}'
-        link = f"/projects/{payload['project']['id']}"
-    elif notification_type == NotificationType.claim_innopoints:
-        reward = payload['application']['actual_hours'] * payload['activity']['reward_rate']
-        title = 'Claim your reward!' if reward != 0 else 'Leave feedback'
-        body = 'Leave feedback on your volunteering work'
-        if reward != 0:
-            body += f' to claim {reward} innopoints'
-        link = f"/projects/{payload['project']['id']}"
-        image = f'/file/{payload["project"]["image_id"]}'
-    elif notification_type == NotificationType.project_review_status_changed:
-        title = 'An administrator has reviewed your project'
-        body = (f'The project {payload["project"]["name"]} was '
-                f'{payload["project"]["review_status"]} by the administrator')
-        link = f"/projects/{payload['project']['id']}"
-        image = f'/file/{payload["project"]["image_id"]}'
-    elif notification_type == NotificationType.added_as_moderator:
-        title = 'Moderator rights'
-        body = f'You have been promoted to moderate {payload["project"]["name"]}'
-        link = f"/projects/{payload['project']['id']}"
-        image = f'/file/{payload["project"]["image_id"]}'
-    elif notification_type == NotificationType.all_feedback_in:
-        title = 'All feedback collected'
-        body = f'All volunteers of {payload["project"]["name"]} have submitted feedback'
-        link = f"/projects/{payload['project']['id']}"
-        image = f'/file/{payload["project"]["image_id"]}'
-    elif notification_type == NotificationType.out_of_stock:
-        title = 'Out of stock'
-        if payload['product']['type'] is None:
-            product_name = payload['product']['name']
-        else:
-            product_name = f"'{payload['product']['name']}' {payload['product']['type']}"
-        if len(payload['variety']['images']) > 0:
-            image = payload['variety']['images'][0]
-        body = f'The {product_name} was sold out'
-        link = f"/products/{payload['product']['id']}"
-    elif notification_type == NotificationType.new_purchase:
-        title = 'New purchase'
-        if payload['product']['type'] is None:
-            product_name = payload['product']['name']
-        else:
-            product_name = f"'{payload['product']['name']}' {payload['product']['type']}"
-        if len(payload['variety']['images']) > 0:
-            image = payload['variety']['images'][0]
-        body = f"{payload['account']['full_name']} has purchased the {product_name}"
-        link = '/dashboard'
-    elif notification_type == NotificationType.project_review_requested:
-        title = 'Project review requested'
-        body = f'The project {payload["project"]["name"]} is ready for review'
-        link = f"/projects/{payload['project']['id']}"
-        image = f'/file/{payload["project"]["image_id"]}'
-    elif notification_type == NotificationType.service:
-        title = 'Administrator\'s message'
-        body = payload['message']
-    elif notification_type == NotificationType.manual_transaction:
-        title = 'Your balance has changed'
-        body = 'The administrator '
-        if payload['transaction']['change'] > 0:
-            body += f"granted {payload['transaction']['change']} innopoints to you"
-        else:
-            body += f"deducted ${-payload['transaction']['change']} innopoints from you"
-    return {
-        'title': title,
-        'body': body,
-        'link': link,
-        'image': image,
-        'type': notification_type.value,
-    }
 
 
 def push(recipient_email: str, notification_type: NotificationType, payload=None):
@@ -132,6 +33,7 @@ def push(recipient_email: str, notification_type: NotificationType, payload=None
     try:
         payload = PayloadSchema().dump(payload.copy())
         data = get_content(notification_type, payload)
+        data['body'] = ''.join(map(remove_links, data['body']))
     except KeyError:
         data = payload
 
